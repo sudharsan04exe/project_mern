24b0fe9d07c29089f9747957579e8cb42508d8dd
grey-4 <sarath.s2022ece@sece.ac.in>
2024-03-13 20:46:14 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -11,17 +11,27 @@ import Music from "./musicpage/music";
 function App() {
   return (
     <>
+<<<<<<< HEAD
 
     <Nav/>
    
 
 
+=======
+    <Nav/>
+    <Home/>
+{/* <Nav/> 
+>>>>>>> bc482fc (updated)
     <Routes>
         <Route path="/" element={
                               <Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Sign />} />
+<<<<<<< HEAD
       </Routes> 
+=======
+       </Routes> */}
+>>>>>>> bc482fc (updated)
     </>
   );
 }
