c27480dbf167a1d52a6151471b28e03517eedb13
grey-4 <sarath.s2022ece@sece.ac.in>
2024-03-13 21:20:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import Nav from "./Nav/Nav";
 import Home from "./Home/home";
 import Login from './login/login';
 import  Sign from './Signup/signup';
-// import Nav from "./Nav/Nav";
 
 import React from "react";
 import { Route,Routes,Link } from "react-router-dom";
-import Music from "./musicpage/music";
 function App() {
   return (
     <>
-    <Nav/>
     <Routes>
-        <Route path="/" element={
-                              <Home />} />
+        <Route path="/" element={<><Nav/>
+                              <Home /></>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Sign />} />
       </Routes> 
